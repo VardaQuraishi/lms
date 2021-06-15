c2c1234805cbcb6408e3561eb759d73c581adabf
Varda Quraishi <vardaquraishi.vq@gmail.com>
2021-06-15 09:06:04 +0500

    delete-order-done

--- src/app/delete-order/delete-order.component.ts
+++ src/app/delete-order/delete-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-delete-order',
@@ -7,9 +8,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteOrderComponent implements OnInit {
 
-  constructor() { }
+  orders: any[];
+
+  constructor() {
+    this.orders =[]
+   }
 
   ngOnInit(): void {
+    axios.get('http://localhost:3000/users/getorders', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,',
+      }
+    }).then((res: any)=> {
+      if (res.data.success) {
+        this.orders = res.data.rec;
+      }
+      else {
+        this.orders = [{
+          name: 'No Order Found',
+          endDate: 'N/A',
+          endTime: 'N/A',
+          _id: null
+        }]
+      }
+    })
+
+
   }
 
+  handleDelete(event: Event, id: any) {
+    event.preventDefault();
+    if (!id) {
+      alert('Cannot Delete')
+    }
+    else {
+
+      axios.delete('http://localhost:3000/users/deleteorder/' + String(id))
+        .then( res => {
+          if (res.data.success) {
+            axios.get('http://localhost:3000/users/getorder', {
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+              }
+            }).then((res: any) => {
+              if (res.data.success) {
+                this.orders = res.data.rec;
+              }
+              else {
+                this.orders = [{
+                  name: 'No Record Found',
+                  enddate: 'N/A',
+                  endtime: 'N/A',
+                  _id: null
+                }]
+              }
+            })
+          }
+        })
+    }
+  }
 }
